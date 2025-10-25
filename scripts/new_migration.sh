50003f0b1b34bd78d68dd72391de172bd6b66f0f
Violet Buse <violet@git.violetbuse.com>
2025-10-25 19:42:34 +0200

    Migrated from custom scripts to a justfile setup

--- scripts/new_migration.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-atlas migrate diff $1 \
-    --dir "file://migrations" \
-    --to "file://schema.sql" \
-    --dev-url "docker://postgres/17/dev?search_path=public"
--- scripts/squirrel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-DATABASE_URL="postgres://postgres:postgres@localhost:5432/postgres" gleam run -m squirrel
